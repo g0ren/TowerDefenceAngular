05915e3adbf0c5c97f3c5b95a6cb2261d66b6f66
g0ren <g0ren@mail.ru>
2023-09-29 22:26:40 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppTowerComponent } from './app-tower/app-tower.component';
 import {NgOptimizedImage} from "@angular/common";
 import { AppEnemyTestComponent } from './app.enemy.test/app.enemy.test.component';
+import { BugComponent } from './bug/bug.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppTowerComponent,
-    AppEnemyTestComponent
+    AppEnemyTestComponent,
+    BugComponent
   ],
   imports: [
     BrowserModule,
